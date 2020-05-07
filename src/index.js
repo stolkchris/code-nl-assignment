893e06bcc3ad871a1dd5a952c068dc22bd717366
Christopher Stolk <stolkchris@gmail.com>
2020-05-07 16:06:32 +0200

    feat: add simple listing of stats of anagrams

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* istanbul-ignore */
 const {program} = require('commander');
 const AnagramFileParser = require('./AnagramFileParser');
 
@@ -8,6 +9,12 @@ program
       AnagramFileParser
           .parse(file)
           .then((anagrams) => {
+            console.table({
+              'Unique words found': [anagrams.length],
+              'Total words': [
+                anagrams.map((set) => [...set]).join().split(',').length,
+              ],
+            });
             console.table(anagrams.map((set) => {
               return [...set];
             }));
